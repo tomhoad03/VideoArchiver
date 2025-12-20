c3f673dd85d1e4ce33f5e5357a3803b2e86cbba8
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2025-12-20 21:33:46 +0000

    N/A Simplification

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) throws Exception {
         downloadFromImportedUrls();
+        downloadVideo("www.youtube.com/watch?v=Bq-tjhfSLQA");
     }
 
     /**
