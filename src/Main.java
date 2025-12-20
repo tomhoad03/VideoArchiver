775677affd572209e894104902dd64d1b893aaf2
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2025-12-20 21:28:21 +0000

    N/A Simplification

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,33 @@ import java.nio.file.StandardOpenOption;
 
 public class Main {
     private static final String YTDLP_PATH = Path.of("yt-dlp.exe").toAbsolutePath().toString();
+    private static final String ARCHIVE_URL = "https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/";
     private static final Path IMPORTED_URLS_PATH = Path.of("imported_urls").toAbsolutePath();
     private static final Path EXPORTED_URLS_PATH = Path.of("exported_urls");
     private static final String EXPORTED_VIDEOS_PATH = "videos/%(title)s.%(ext)s";
+    private static final boolean IS_DRY_RUN = true;
 
-    public static void main(String[] args) throws IOException {
+    /**
+     * Main class for VideoArchiver
+     */
+    public static void main(String[] args) throws Exception {
+        downloadFromImportedUrls();
+    }
+
+    /**
+     * Download all the videos from the imported_urls file
+     *
+     * @throws Exception if a download fails
+     */
+    private static void downloadFromImportedUrls() throws Exception {
         Files.lines(IMPORTED_URLS_PATH).forEach(videoUrl -> {
             try {
                 // Create wayback url
                 String videoId = videoUrl.substring(videoUrl.indexOf("?v=") + 3);
-                String waybackUrl = "https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/" + videoId;
+                String waybackUrl = ARCHIVE_URL + videoId;
 
                 // Download the video
-                int exitCode = downloadVideo(waybackUrl, true);
+                int exitCode = downloadVideo(waybackUrl);
 
                 // Write out the successful url
                 if (exitCode == 0)
@@ -26,9 +40,16 @@ public class Main {
         });
     }
 
-    private static int downloadVideo(String videoString, boolean isDryRun) throws Exception {
+    /**
+     * Download a single video using yt-dlp
+     *
+     * @param videoString the string of the video to download
+     * @return The exit code from downloading a video
+     * @throws Exception if the download fails
+     */
+    private static int downloadVideo(String videoString) throws Exception {
         // Dry run condition
-        String dryRunFlag = isDryRun ? "--simulate" : "";
+        String dryRunFlag = IS_DRY_RUN ? "--simulate" : "";
 
         // Run yt-dlp
         Process process = new ProcessBuilder(YTDLP_PATH, "-o", EXPORTED_VIDEOS_PATH, dryRunFlag, "-f", "best", videoString).start();
