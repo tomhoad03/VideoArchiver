4eece29477b7181b1ac513b2618d21c06bff0cdc
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2025-12-20 21:14:12 +0000

    N/A Simplification

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,44 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Main {
+    private static final String YTDLP_PATH = Path.of("src/yt-dlp.exe").toAbsolutePath().toString();
+    private static final Path IMPORTED_URLS_PATH = Path.of("imported_urls").toAbsolutePath();
+    private static final Path EXPORTED_URLS_PATH = Path.of("exported_urls");
+    private static final String EXPORTED_VIDEOS_PATH = "videos/%(title)s.%(ext)s";
+
     public static void main(String[] args) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("extracted_urls"));
-
-        Files.walk(Paths.get("exports")).filter(Files::isRegularFile).forEach(file -> {
+        Files.lines(IMPORTED_URLS_PATH).forEach(videoUrl -> {
             try {
-                Files.lines(file.toAbsolutePath()).forEach(videoUrl -> {
-                    try {
-                        // Create wayback url
-                        String videoId = videoUrl.substring(videoUrl.indexOf("?v=") + 3);
-                        String waybackUrl = "https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/" + videoId;
+                // Create wayback url
+                String videoId = videoUrl.substring(videoUrl.indexOf("?v=") + 3);
+                String waybackUrl = "https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/" + videoId;
 
-                        // Download the video
-                        int exitCode = downloadVideo(waybackUrl);
+                // Download the video
+                int exitCode = downloadVideo(waybackUrl, true);
 
-                        // Write the successful url
-                        if (exitCode == 0) {
-                            writer.write(videoUrl);
-                            writer.newLine();
-                            System.out.println("Exported " + videoUrl);
-                        }
-                    } catch (Exception ignored) {}
-                });
+                // Write out the successful url
+                if (exitCode == 0)
+                    Files.write(EXPORTED_URLS_PATH, (videoUrl + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
             } catch (Exception ignored) {}
         });
     }
 
-    private static int downloadVideo(String videoString) throws IOException, InterruptedException {
-        // Output directory
-        String outputDir = "videos/%(title)s.%(ext)s";
+    private static int downloadVideo(String videoString, boolean isDryRun) throws Exception {
+        // Dry run condition
+        String dryRunFlag = isDryRun ? "--simulate" : "";
 
         // Run yt-dlp
-        File ytDlp = new File("src/yt-dlp.exe");
-        Process process = new ProcessBuilder(ytDlp.getAbsolutePath(), "-o", outputDir, "-f", "best", videoString).start();
+        Process process = new ProcessBuilder(YTDLP_PATH, "-o", EXPORTED_VIDEOS_PATH, dryRunFlag, "-f", "best", videoString).start();
 
         // Read the output of the process
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
+        reader.lines().forEach(System.out::println);
 
-        // Download is finished
+        // Download is finished, return status code
         return process.waitFor();
     }
 }
